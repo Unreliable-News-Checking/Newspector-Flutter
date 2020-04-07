b9a0cf144fb6b07e2be269033e79461677deecde
Ahmet Ayrancioglu <astodb9@gmail.com>
2020-04-07 22:28:20 +0300

    moved the widgets to their own files

--- lib/models/news_feed.dart
+++ lib/models/news_feed.dart
@@ -4,4 +4,12 @@ class NewsFeed {
   List<NewsGroup> newsGroups;
 
   NewsFeed();
+
+  int getGroupCount() {
+    return newsGroups.length;
+  }
+
+  NewsGroup getNewsGroup(int index){
+    return newsGroups[index];
+  }
 }
