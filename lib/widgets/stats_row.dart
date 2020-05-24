ed46b7ce210394a3cc26c143abdf452d8ae25e24
Deniz Dalkılıç <denizdalkilic1998@gmail.com>
2020-05-24 23:44:11 +0300

    Added StatsRow Widget

---
+++ lib/widgets/stats_row.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:newspector_flutter/application_constants.dart' as app_const;
+
+class StatsRow extends StatelessWidget {
+  final Widget icon;
+  final String label;
+
+  const StatsRow({Key key, @required this.icon, @required this.label})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            child: FittedBox(
+              child: icon,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 15),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              shadows: app_const.shadowsForWhiteWidgets(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
